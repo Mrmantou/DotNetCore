1174adfb25345a7c6b16ef606824bbcd10b947e8
MrMantou <albert_nchu@163.com>
2018-07-10 10:19:08 +0800

    添加类库的单元测试和console使用类库

---
+++ ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UtilityLibraries;
+
+namespace ShowCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 0;
+            do
+            {
+                if (row == 0 || row >= 25)
+                {
+                    ResetConsole();
+                }
+
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Input: {input} {"Begins with uppercase?",1}: " + $"{(input.StartsWithUpper() ? "Yes" : "No")}\n");
+
+                row += 3;
+            }
+            while (true);
+            return;
+
+            void ResetConsole()
+            {
+                if (row > 0)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+                Console.WriteLine("\nPress <Enter> onlu to exit; otherwise, enter a string and press <Enter>:\n");
+                row = 3;
+            }
+        }
+    }
+}
---
+++ ClassLibraryProjects/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartWithUpper()
+        {
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+
+            foreach (var item in words)
+            {
+                bool result = item.StartsWithUpper();
+                Assert.IsTrue(result);
+            }
+        }
+
+        [TestMethod]
+        public void TestDoseNotStartWithUpper()
+        {
+            string[] words = { "alphabet", "zebra", "aBC", "aθήνα", "mосква" };
+
+            foreach (var item in words)
+            {
+                bool result = item.StartsWithUpper();
+
+                Assert.IsFalse(result);
+            }
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            string[] words = { string.Empty, null };
+
+            foreach (var item in words)
+            {
+                bool result = item.StartsWithUpper();
+
+                Assert.IsFalse(result);
+            }
+        }
+    }
+}
