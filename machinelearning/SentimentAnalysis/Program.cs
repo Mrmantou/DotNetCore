f247f2a511b60d0081e3713d82410d690e32228e
MrMantou <albert_nchu@163.com>
2018-07-13 13:11:41 +0800

    添加

---
+++ machinelearning/SentimentAnalysis/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ML;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysis
+{
+    class Program
+    {
+        private static string AppPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string TrainDataPath => Path.Combine(AppPath, "datasets", "sentiment-imdb-train.txt");
+        private static string RestDataPath => Path.Combine(AppPath, "datasets", "sentiment-yelp-test.txt");
+        private static string ModelPath => Path.Combine(AppPath, "SentimentModel.zip");
+
+        static async void Main(string[] args)
+        {
+            var model = await TrainModelAsync();
+        }
+
+        private static async Task<PredictionModel<SentimentData, SentimentPrediction>> TrainModelAsync()
+        {
+            var pipeLine = new LearningPipeline();
+
+           
+        }
+    }
+}
---
+++ machinelearning/SentimentAnalysis/SentimentData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SentimentAnalysis
+{
+    public class SentimentData
+    {
+        [Column("0")]
+        public string SentimentText;
+
+        [Column("1",name:"Label")]
+        public float Sentiment;
+    }
+}
