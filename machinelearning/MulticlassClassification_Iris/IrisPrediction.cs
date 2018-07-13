86ad5b671e2e7c99243ee5a92d01d90a582a9fd6
MrMantou <albert_nchu@163.com>
2018-07-13 18:00:12 +0800

    add code

---
+++ machinelearning/MulticlassClassification_Iris/IrisPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MulticlassClassification_Iris
+{
+   public class IrisPrediction
+    {
+        [ColumnName("Score")]
+        public float[] Score;
+    }
+}
--- machinelearning/MulticlassClassification_Iris/Program.cs
+++ machinelearning/MulticlassClassification_Iris/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace MulticlassClassification_Iris
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string TrainDataPath => Path.Combine(AppPath, "datasets", "iris-train.txt");
+        private static string TestDataPath => Path.Combine(AppPath, "datasets", "iris-test.txt");
+        private static string ModelPath => Path.Combine(AppPath, "IrisModel.zip");
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var model = await TrainAsync();
+        }
+
+        private static async Task<PredictionModel<IrisData, IrisPrediction>> TrainAsync()
+        {
+            var pipeLine = new LearningPipeline {
+                new TextLoader(TrainDataPath).CreateFrom<IrisData>(),
+                new ColumnConcatenator("Features","SepalLength","SepalWidth","PetalLength","PetalWidth"),
+                new StochasticDualCoordinateAscentClassifier()
+            };
+
+            Console.WriteLine("=============== Training model ===============");
+
+            var model = pipeLine.Train<IrisData, IrisPrediction>();
+
+            await model.WriteAsync(ModelPath);
+
+            Console.WriteLine("=============== End training ===============");
+            Console.WriteLine("The model is saved to {0}", ModelPath);
+
+            return model;
         }
     }
 }
