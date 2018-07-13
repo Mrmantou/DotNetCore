d0b37dad943c67313dc0f8beced6d051e1aea943
MrMantou <albert_nchu@163.com>
2018-07-13 14:03:09 +0800

    修改情感分析，完成代码

--- machinelearning/SentimentAnalysis/Program.cs
+++ machinelearning/SentimentAnalysis/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
 using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SentimentAnalysis
@@ -9,19 +14,63 @@ namespace SentimentAnalysis
     {
         private static string AppPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
         private static string TrainDataPath => Path.Combine(AppPath, "datasets", "sentiment-imdb-train.txt");
-        private static string RestDataPath => Path.Combine(AppPath, "datasets", "sentiment-yelp-test.txt");
+        private static string TestDataPath => Path.Combine(AppPath, "datasets", "sentiment-yelp-test.txt");
         private static string ModelPath => Path.Combine(AppPath, "SentimentModel.zip");
 
-        static async void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var model = await TrainModelAsync();
+
+            Evaluate(model);
+
+            var predictions = model.Predict(TestSentimentData.Sentiments);
+
+            var sentimentsAndPredictions = TestSentimentData.Sentiments.Zip(predictions, (sentiment, prediction) => (sentiment, prediction));
+
+            foreach (var item in sentimentsAndPredictions)
+            {
+                Console.WriteLine($"Sentiment: {item.sentiment.SentimentText}|Prediction: {(item.prediction.Sentiment ? "Positive" : "Negative")}");
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Evaluate(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            var testData = new TextLoader(TestDataPath).CreateFrom<SentimentData>();
+
+            var evaluator = new BinaryClassificationEvaluator();
+
+            Console.WriteLine("=============== Evaluating model ===============");
+
+            var metrics = evaluator.Evaluate(model, testData);
+
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End evaluating ===============");
+            Console.WriteLine();
         }
 
         private static async Task<PredictionModel<SentimentData, SentimentPrediction>> TrainModelAsync()
         {
             var pipeLine = new LearningPipeline();
 
-           
+            pipeLine.Add(new TextLoader(TrainDataPath).CreateFrom<SentimentData>());
+
+            pipeLine.Add(new TextFeaturizer("Features", "SentimentText"));
+
+            pipeLine.Add(new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 });
+
+            Console.WriteLine("=============== Training Model =============== ");
+            var model = pipeLine.Train<SentimentData, SentimentPrediction>();
+
+            await model.WriteAsync(ModelPath);
+
+            Console.WriteLine("=============== End training ===============");
+            Console.WriteLine("The model is saved to {0}", ModelPath);
+
+            return model;
         }
     }
 }
