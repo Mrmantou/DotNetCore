17f081e97564a3a42de353810bacca131bbb640f
MrMantou <albert_nchu@163.com>
2018-07-20 13:39:02 +0800

    添加登录界面

--- Albert.Echo.MVC.Web.UI/Startup.cs
+++ Albert.Echo.MVC.Web.UI/Startup.cs
@@ -56,7 +56,7 @@ namespace Albert.Echo.MVC.Web.UI
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
