b066ed7e0fecf54783c8e09251aeb360d446ccbc
MrMantou <albert_nchu@163.com>
2018-07-20 10:23:51 +0800

    添加 asp.net core mvc

---
+++ Albert.Echo.MVC.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Albert.Echo.MVC.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
