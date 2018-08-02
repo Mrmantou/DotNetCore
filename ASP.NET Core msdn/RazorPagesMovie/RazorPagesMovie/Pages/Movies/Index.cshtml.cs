cf5bf62c0939618844c6cc166aa584f09b760fc9
MrMantou <albert_nchu@163.com>
2018-08-02 15:27:00 +0800

    web app 电影demo教程完善
    Signed-off-by: MrMantou <albert_nchu@163.com>

--- ASP.NET Core msdn/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ ASP.NET Core msdn/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -18,11 +19,31 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string movieGenre, string searchString)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(x => x.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(e => e.Genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToArrayAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
--- ASP.NET Core msdn/RazorPagesMovie/RazorPagesMovie/Program.cs
+++ ASP.NET Core msdn/RazorPagesMovie/RazorPagesMovie/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -14,7 +17,26 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<RazorPagesMovieContext>();
+                    context.Database.Migrate();//A production app would not call Database.Migrate. It's added to the preceding code to prevent the following exception when Update-Database has not been run
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
