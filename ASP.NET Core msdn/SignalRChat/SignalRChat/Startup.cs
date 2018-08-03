376b968305746f94ff3b4770bf3d72dc28c84694
MrMantou <albert_nchu@163.com>
2018-08-03 10:30:39 +0800

    修改bug

--- ASP.NET Core msdn/SignalRChat/SignalRChat/Startup.cs
+++ ASP.NET Core msdn/SignalRChat/SignalRChat/Startup.cs
@@ -56,7 +56,7 @@ namespace SignalRChat
             app.UseCookiePolicy();
             app.UseSignalR(routes =>
             {
-                routes.MapHub<ChatHub>("/charHub");
+                routes.MapHub<ChatHub>("/chatHub");
             });
 
             app.UseMvc();
