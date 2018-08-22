e6fbbd9b67bcf510385d6f3262ba96ed0c02f491
MrMantou <albert_nchu@163.com>
2018-08-22 17:59:36 +0800

    添加配置文件demo
    Signed-off-by: MrMantou <albert_nchu@163.com>

---
+++ ASP.NET Core msdn/WebApplication/InMemory/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace InMemory
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, string>
+            {
+                 {"Profile:MachineName", "Rick"},
+                 {"App:MainWindow:Height", "11"},
+                 {"App:MainWindow:Width", "11"},
+                 {"App:MainWindow:Top", "11"},
+                 {"App:MainWindow:Left", "11"}
+            };
+
+            var builder = new ConfigurationBuilder();
+
+            builder.AddInMemoryCollection(dict);
+
+            Configuration = builder.Build();
+
+            Console.WriteLine($"Hello {Configuration["Profile:MachineName"]}");
+
+            var left = Configuration.GetValue<int>("App:MainWindow:Left", 80);
+            Console.WriteLine($"Left-> {left}");
+
+            var window = new MyWindow();
+            // Bind requrires NuGet package
+            // Microsoft.Extensions.Configuration.Binder
+            Configuration.GetSection("App:MainWindow").Bind(window);
+            Console.WriteLine($"Left {window.Left}");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+        public class MyWindow
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public int Top { get; set; }
+            public int Left { get; set; }
+        }
+    }
+}
