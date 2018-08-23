4ccbbadc1419db2379969ea3b1bcfa4e7bc7c1ac
MrMantou <albert_nchu@163.com>
2018-08-23 09:49:08 +0800

    添加配置文件复杂对象绑定

---
+++ ASP.NET Core msdn/WebApplication/ObjectGraph/AppSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectGraph
+{
+    public class AppSettings
+    {
+        public Window Window { get; set; }
+        public Connection Connection { get; set; }
+        public Profile Profile { get; set; }
+    }
+
+    public class Window
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+
+    public class Connection
+    {
+        public string Value { get; set; }
+    }
+
+    public class Profile
+    {
+        public string Machine { get; set; }
+    }
+}
---
+++ ASP.NET Core msdn/WebApplication/ObjectGraph/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace ObjectGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            var appConfig = new AppSettings();
+
+            //  config.GetSection("App").Bind(appConfig);
+            appConfig = config.GetSection("App").Get<AppSettings>();
+
+            Console.WriteLine($"Height {appConfig.Window.Height}");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
