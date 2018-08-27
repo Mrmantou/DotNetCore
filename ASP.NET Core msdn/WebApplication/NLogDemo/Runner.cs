c06979eee83f85d8dc6e6b1fe86bc7a0c90b15c3
MrMantou <albert_nchu@163.com>
2018-08-27 16:38:02 +0800

    完善NLog配置文件
    Signed-off-by: MrMantou <albert_nchu@163.com>

---
+++ ASP.NET Core msdn/WebApplication/NLogDemo/Runner.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NLogDemo
+{
+    public class Runner
+    {
+        private readonly ILogger<Runner> logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            logger.LogDebug(20, "Doing hard work! {Action}", name);
+            logger.LogInformation(20, "Doing hard work! {Action}", name);
+        }
+    }
+}
