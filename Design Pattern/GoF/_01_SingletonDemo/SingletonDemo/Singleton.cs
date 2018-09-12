df3e289ef0b23c1a8e382984e2549b464a35c553
MrMantou <albert_nchu@163.com>
2018-09-12 20:35:28 +0800

    添加单例模式demo
    Signed-off-by: MrMantou <albert_nchu@163.com>

---
+++ Design Pattern/GoF/_01_SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+    /// <summary>
+    /// 单线程Singleton
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
