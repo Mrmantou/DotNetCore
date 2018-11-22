f5326897beffdf48dff556cff143f6b41cae9966
MrMantou <albert_nchu@163.com>
2018-11-22 09:54:15 +0800

    ef core with sqlite console

---
+++ EntityFrameworkCore/_01_ConsoleApp.SQLite/ConsoleApp.SQLite/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp.SQLite
+{
+    /// <summary>
+    /// nuget package:
+    ///  - Microsoft.EntityFrameworkCore.Sqlite
+    ///  - Microsoft.EntityFrameworkCore.Design
+    ///  - Microsoft.EntityFrameworkCore.Tools
+    ///  Run from Visual Studio
+    ///  To run this sample from Visual Studio, you must set the working directory manually to be the root of the project. If you don't set the working directory, the following Microsoft.Data.Sqlite.SqliteException is thrown: SQLite Error 1: 'no such table: Blogs'.
+    ///To set the working directory:
+    /// - In Solution Explorer, right click the project and then select Properties.
+    /// - Select the Debug tab in the left pane.
+    /// - Set Working directory to the project directory.
+    /// - Save the changes.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BloggingContext())
+            {
+                context.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = context.SaveChanges();
+                Console.WriteLine($"{count} records saved to database");
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+
+                foreach (var blog in context.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
