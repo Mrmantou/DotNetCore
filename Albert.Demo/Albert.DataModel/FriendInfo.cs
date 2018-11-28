13d79a830053d37b9113c900ce69871cf860efd9
MrMantou <albert_nchu@163.com>
2018-11-28 16:18:57 +0800

    添加数据模型程序集，ef程序集

---
+++ Albert.Demo/Albert.DataModel/FriendInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Albert.DataModel
+{
+    public class FriendInfo
+    {
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string NickName { get; set; }
+        /// <summary>
+        /// 关系
+        /// </summary>
+        public RelationType RelationType { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ Albert.Demo/Albert.DataModel/RelationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albert.DataModel
+{
+    public enum RelationType
+    {
+        /// <summary>
+        /// 同学
+        /// </summary>
+        Classmate = 1,
+        /// <summary>
+        /// 同事
+        /// </summary>
+        Workmate = 2,
+        /// <summary>
+        /// 朋友
+        /// </summary>
+        Friend = 3,
+        /// <summary>
+        /// 其他
+        /// </summary>
+        Others = 4
+    }
+}
