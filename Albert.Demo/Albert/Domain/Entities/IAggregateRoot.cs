41b81ab16024bc2958bafade8ad647033790ad91
MrMantou <albert_nchu@163.com>
2018-12-01 10:47:07 +0800

    添加聚合跟和接口

---
+++ Albert.Demo/Albert/Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Albert.Domain.Entities
+{
+    public interface IAggregateRoot : IEntity<int>
+    {
+
+    }
+    
+    public interface IAggregateRoot<TPrimaryKey> : IEntity<TPrimaryKey>
+    {
+
+    }
+}
