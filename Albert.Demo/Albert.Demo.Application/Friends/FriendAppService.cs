ef4d81c1057c3d8b3b889faec0f178c71f5d2735
MrMantou <albert_nchu@163.com>
2018-12-09 16:01:24 +0800

    修改获取friend集合为异步方法
    Signed-off-by: MrMantou <albert_nchu@163.com>

--- Albert.Demo/Albert.Demo.Application/Friends/FriendAppService.cs
+++ Albert.Demo/Albert.Demo.Application/Friends/FriendAppService.cs
@@ -6,20 +6,25 @@ using Albert.Demo.Domain.Friends;
 using Albert.Domain.Repositories;
 using Albert.Linq.Extensions;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Albert.Demo.Application.Friends
 {
     public class FriendAppService : IFriendAppService
     {
         private readonly IRepository<Friend> repository;
+
         public FriendAppService(IRepository<Friend> repository)
         {
             this.repository = repository;
         }
 
-        public List<Friend> GetFriends(GetFriendArg input)
+        public async Task<List<Friend>> GetFriends(GetFriendArg input)
         {
-            return repository.GetAll().WhereIf(!string.IsNullOrEmpty(input.NickName), f => f.NickName == input.NickName).ToList();
+            return await repository.GetAll()
+                .WhereIf(!string.IsNullOrEmpty(input.NickName), f => f.NickName == input.NickName)
+                .ToListAsync();
         }
     }
 }
--- Albert.Demo/Albert.Demo.Application/Friends/IFriendAppService.cs
+++ Albert.Demo/Albert.Demo.Application/Friends/IFriendAppService.cs
@@ -3,11 +3,12 @@ using Albert.Demo.Domain.Friends;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Albert.Demo.Application.Friends
 {
     public interface IFriendAppService
     {
-        List<Friend> GetFriends(GetFriendArg input);
+        Task<List<Friend>> GetFriends(GetFriendArg input);
     }
 }
