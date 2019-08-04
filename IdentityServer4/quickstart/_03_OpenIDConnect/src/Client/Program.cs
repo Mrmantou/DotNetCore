4d5f8fc19950d2be81b01a18ac991b142114d68e
MrMantou <albert_nchu@163.com>
2019-08-04 14:06:35 +0800

    add api and client in demo 1, and mvc client in demo 3

---
+++ IdentityServer4/quickstart/_03_OpenIDConnect/src/Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var discover = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (discover.IsError)
+            {
+                Console.WriteLine(discover.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = discover.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var callServerClient = new HttpClient();
+            callServerClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await callServerClient.GetAsync("http://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.WriteLine("press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IdentityServer4/quickstart/_03_OpenIDConnect/src/MvcClient/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options => // add the authentication services to DI
+                {
+                    options.DefaultScheme = "Cookies"; options.DefaultChallengeScheme = "oidc";
+                })
+                .AddCookie("Cookies") // add handler that can process cookies.
+                .AddOpenIdConnect("iodc", options => // configure the handler that perform the OpenID Connection protocol.
+                {
+                    options.Authority = "http://localhost:5000"; // indicates that we are trusting IdentityServer.
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "mvc"; // identity this client via the ClientId(mvc)
+                    options.SaveTokens = true; // used to persist the token from IdentityServer in the cookie
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseAuthentication(); // The authentication middleware should be added before the Mvc in the pipeline.
+
+            app.UseStaticFiles();
+            //app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
