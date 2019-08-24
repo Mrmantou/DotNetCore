fa7428cf074603e477780f897ff185810389cb39
Mrmantou <albert_nchu@163.com>
2019-08-24 10:19:59 +0800

    add identity scaffold

---
+++ Security and Identity/_02_WebApp/EmptyWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EmptyWebApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmptyWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace EmptyWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EmptyWebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EmptyWebAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<EmptyWebAppContext>();
+            });
+        }
+    }
+}
