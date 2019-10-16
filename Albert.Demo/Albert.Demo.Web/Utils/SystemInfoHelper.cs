a9e75105646dfcf2134f644d3cc95545deb18fab
Mrmantou <albert_nchu@163.com>
2019-10-16 12:58:19 +0800

    add platform and framework info

---
+++ Albert.Demo/Albert.Demo.Web/Utils/SystemInfoHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Albert.Demo.Web.Utils
+{
+    public class SystemInfoHelper
+    {
+        public static string PlatformAndFrameworkInfo()
+        {
+            return $"{RuntimeInformation.FrameworkDescription} on {RuntimeInformation.OSDescription}";
+        }
+    }
+}
