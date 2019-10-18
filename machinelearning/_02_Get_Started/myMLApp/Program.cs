b6ed27c665d7bcd0fde223c27206e5e0f8d467b3
Mrmantou <albert_nchu@163.com>
2019-10-18 18:17:10 +0800

    add get started ml.net demo

---
+++ machinelearning/_02_Get_Started/myMLApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using MachineLearningML.Model;
+using System;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+
+            input.SentimentText = "That is rude.";
+
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText}\n Is Toxic: {result.Prediction} {result.Score}");
+
+            Console.WriteLine("Get Started ML.NET!");
+
+            Console.WriteLine("Press any key to exit......");
+            Console.ReadKey();
+        }
+    }
+}
