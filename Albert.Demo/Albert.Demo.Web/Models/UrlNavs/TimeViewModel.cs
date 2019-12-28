3716434403c2829b2fdd9468f9a9ff4cb80e8df7
Mrmantou <albert_nchu@163.com>
2019-12-28 22:35:12 +0800

    add index in urlnav time view

--- Albert.Demo/Albert.Demo.Web/Models/UrlNavs/TimeViewModel.cs
+++ Albert.Demo/Albert.Demo.Web/Models/UrlNavs/TimeViewModel.cs
@@ -5,6 +5,8 @@ namespace Albert.Demo.Web.Models.UrlNavs
 {
     public class TimeViewModel
     {
+        private int index = 0;
+        public int Index { get { return index++; } }
         public IReadOnlyList<TimeDto> TimeDtos { get; set; }
         public TimeViewModel(IReadOnlyList<TimeDto> timeDtos)
         {
