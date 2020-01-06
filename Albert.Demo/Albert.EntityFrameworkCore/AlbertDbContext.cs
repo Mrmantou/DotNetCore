73bd46ca7406bb0486a861eaa87da6022a28e29d
Mrmantou <albert_nchu@163.com>
2020-01-06 21:17:00 +0800

    simplify ef model creating

--- Albert.Demo/Albert.EntityFrameworkCore/AlbertDbContext.cs
+++ Albert.Demo/Albert.EntityFrameworkCore/AlbertDbContext.cs
@@ -17,19 +17,8 @@ namespace Albert.EntityFrameworkCore
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var typesToRegister = configAssembly.GetTypes()
-                .Where(type => !string.IsNullOrEmpty(type.Namespace))
-                .Where(type => type.BaseType != null
-                    && type.BaseType.IsGenericType
-                    && type.BaseType.GetGenericTypeDefinition() == typeof(AlbertEntityTypeConfiguration<>));
-
-            foreach (var type in typesToRegister)
-            {
-                dynamic configurationInstance = Activator.CreateInstance(type);
-                modelBuilder.ApplyConfiguration(configurationInstance);
-            }
-
             base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(configAssembly);
         }
     }
 }
