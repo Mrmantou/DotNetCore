26a6a7a59b521a15684b0d5698b67c575459299b
Mrmantou <albert_nchu@163.com>
2020-01-27 18:18:30 +0800

    print thread id

--- dotnet core/DonetCoreDemo/TaskYieldDemo/AwaitYieldDemo.cs
+++ dotnet core/DonetCoreDemo/TaskYieldDemo/AwaitYieldDemo.cs
@@ -21,25 +21,24 @@ namespace TaskYieldDemo
             {
                 tasks[j].Start();
             }
-            var id = Thread.GetCurrentProcessorId();
+            Console.WriteLine($"main thread ---{Thread.CurrentThread.ManagedThreadId}");
             //我来吃火锅了
             GotoShuDaXiaEatHotPot();
             //人们陆续来吃火锅
             for (int j = 10; j < tasks.Length; j++)
             {
-                var loopId = Thread.GetCurrentProcessorId();
                 tasks[j].Start();
             }
         }
         private void PersonEatHot(object personNo)
         {
-            Console.WriteLine($"I am No.{personNo} person.I enter restaurant");
+            Console.WriteLine($"I am No.{personNo} person.I enter restaurant ---{Thread.CurrentThread.ManagedThreadId}");
             Thread.Sleep(1000);   //eating
-            Console.WriteLine($"I am No.{ personNo } person.I eat completed.");
+            Console.WriteLine($"I am No.{ personNo } person.I eat completed. ---{Thread.CurrentThread.ManagedThreadId}");
         }
         private async Task GotoShuDaXiaEatHotPot()
         {
-            Console.WriteLine($"I get a waiting card.");
+            Console.WriteLine($"I get a waiting card. ---{Thread.CurrentThread.ManagedThreadId}");
             await Task.Yield();  //到店了 先排个号 Creates an awaitable task that asynchronously yields back to the current context when awaited.
             WaitMyPartnerJoin(5);  //等待我的5个小伙伴集合
             await EatingHotPot();   //开始吃火锅
@@ -48,23 +47,23 @@ namespace TaskYieldDemo
         {
             await Task.Run(() =>
             {
-                Console.WriteLine("eating hot pot with my friends");
+                Console.WriteLine($"eating hot pot with my friends ---{Thread.CurrentThread.ManagedThreadId}");
                 Thread.Sleep(1000);
-                Console.WriteLine("Completed : eating hot pot with my friends");
+                Console.WriteLine($"Completed : eating hot pot with my friends ---{Thread.CurrentThread.ManagedThreadId}");
             });
         }
         private void WaitMyPartnerJoin(int partnerNum)
         {
-            var id = Thread.GetCurrentProcessorId();
+            var id = Thread.CurrentThread.ManagedThreadId;
             Console.WriteLine("Waiting my partner join.");
             for (int i = 0; i < partnerNum; i++)
             {
                 for (int j = 0; j < 1000000; j++)
                 {
                 }
-                Console.WriteLine($"no.{i} friend join.");
+                Console.WriteLine($"no.{i} friend join. ---{Thread.CurrentThread.ManagedThreadId}");
             }
-            Console.WriteLine("everyone is here.");
+            Console.WriteLine($"everyone is here. ---{Thread.CurrentThread.ManagedThreadId}");
         }
     }
 }
