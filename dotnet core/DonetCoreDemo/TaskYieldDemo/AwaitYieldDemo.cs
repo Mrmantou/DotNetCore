60d6fdab40029390f42cbf7ff95b134b5b76a00f
Mrmantou <albert_nchu@163.com>
2020-01-27 13:16:55 +0800

    add a task yield demo

---
+++ dotnet core/DonetCoreDemo/TaskYieldDemo/AwaitYieldDemo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskYieldDemo
+{
+    public class AwaitYieldDemo
+    {
+        public void MockHotPotRestaurant()
+        {
+            Task[] tasks = new Task[20];
+            //构建一批吃火锅的人
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = new Task(PersonEatHot, i);
+            }
+            //人们陆续来吃火锅
+            for (int j = 0; j < tasks.Length / 2; j++)
+            {
+                tasks[j].Start();
+            }
+            var id = Thread.GetCurrentProcessorId();
+            //我来吃火锅了
+            GotoShuDaXiaEatHotPot();
+            //人们陆续来吃火锅
+            for (int j = 10; j < tasks.Length; j++)
+            {
+                var loopId = Thread.GetCurrentProcessorId();
+                tasks[j].Start();
+            }
+        }
+        private void PersonEatHot(object personNo)
+        {
+            Console.WriteLine($"I am No.{personNo} person.I enter restaurant");
+            Thread.Sleep(1000);   //eating
+            Console.WriteLine($"I am No.{ personNo } person.I eat completed.");
+        }
+        private async Task GotoShuDaXiaEatHotPot()
+        {
+            Console.WriteLine($"I get a waiting card.");
+            await Task.Yield();  //到店了 先排个号 Creates an awaitable task that asynchronously yields back to the current context when awaited.
+            WaitMyPartnerJoin(5);  //等待我的5个小伙伴集合
+            await EatingHotPot();   //开始吃火锅
+        }
+        private async Task EatingHotPot()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("eating hot pot with my friends");
+                Thread.Sleep(1000);
+                Console.WriteLine("Completed : eating hot pot with my friends");
+            });
+        }
+        private void WaitMyPartnerJoin(int partnerNum)
+        {
+            var id = Thread.GetCurrentProcessorId();
+            Console.WriteLine("Waiting my partner join.");
+            for (int i = 0; i < partnerNum; i++)
+            {
+                for (int j = 0; j < 1000000; j++)
+                {
+                }
+                Console.WriteLine($"no.{i} friend join.");
+            }
+            Console.WriteLine("everyone is here.");
+        }
+    }
+}
---
+++ dotnet core/DonetCoreDemo/TaskYieldDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskYieldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new AwaitYieldDemo().MockHotPotRestaurant();
+
+            Console.WriteLine("press any key to exit......");
+            Console.ReadKey();
+        }
+    }
+}
