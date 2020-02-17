44491d417cd31f9e6f09928b4f15015ce5c45b0e
Mrmantou <albert_nchu@163.com>
2020-02-17 16:40:56 +0800

    add comments

--- asp.net core/WebApplication/WebMvcApp/Startup.cs
+++ asp.net core/WebApplication/WebMvcApp/Startup.cs
@@ -36,12 +36,21 @@ namespace WebMvcApp
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
             app.UseStaticFiles();
 
+            // ASP.NET Core 3.0 终结点(端点)路由
+            // HTTP请求跟地址后面的那部分，localhost:5000/home/index
+            //                          终结点/home/index
+            // 在两个中间件 UseRouting UseEndpoints 之间可以设置其他中间件，可以利用端点路由中间件的路由信息，处理一些其他逻辑
+            // 将解析出来的路由信息写进请i去上下文对象(HttpContext)
             app.UseRouting();
 
+            // 身份验证 
+            // 根据路由来进行
             app.UseAuthorization();
 
+            // 端点中间件
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
