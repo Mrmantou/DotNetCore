2fababe051712f8de767c49382d5bfd1c23999e4
Mrmantou <albert_nchu@163.com>
2020-02-17 18:32:19 +0800

    add some demo code

---
+++ asp.net core/WebApplication/WebApp/RequestSetOptionsStartupFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class RequestSetOptionsStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UseMiddleware<RequestSetOptionsMiddleware>();
+                next(app);
+            };
+        }
+    }
+}
--- asp.net core/WebApplication/WebMvcApp/Program.cs
+++ asp.net core/WebApplication/WebMvcApp/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
 
 namespace WebMvcApp
 {
@@ -14,6 +16,8 @@ namespace WebMvcApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            //CreateHostBuilderWithoutStartup(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +25,40 @@ namespace WebMvcApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                });
+
+        public static IHostBuilder CreateHostBuilderWithoutStartup(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddControllersWithViews();
+                    })
+                    .Configure(app =>
+                    {
+                        var loggerFactory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+                        var logger = loggerFactory.CreateLogger<Program>();
+                        var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+                        var config = app.ApplicationServices.GetRequiredService<IConfiguration>();
+
+                        logger.LogInformation("Logged in Configure");
+
+                        if (env.IsDevelopment())
+                        {
+                            app.UseDeveloperExceptionPage();
+                        }
+                        else
+                        {
+                            app.UseExceptionHandler("/Home/Error");
+                            app.UseHsts();
+                        }
+
+                        var configValue = config["MyConfigKey"];
+                    });
                 });
     }
 }
