0a2b41bbb90cb439580d02a01846bf5717965c73
Mrmantou <albert_nchu@163.com>
2020-02-18 17:34:21 +0800

    add service create demo

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_01DependenceInjection/_DependenceInjection_07/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace _DependenceInjection_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ServiceCreateTest();
+            ServiceCreateConflictTest();
+
+            Console.WriteLine("press any key to exit......");
+            Console.ReadKey();
+        }
+
+        private static void ServiceCreateTest()
+        {
+            new ServiceCollection()
+                .AddTransient<IFoo, Foo>()
+                .AddTransient<IBar, Bar>()
+                .AddTransient<IGux, Gux>()
+                .BuildServiceProvider()
+                .GetService<IGux>();
+
+
+        }
+
+        private static void ServiceCreateConflictTest()
+        {
+            new ServiceCollection()
+                .AddTransient<IFoo, Foo>()
+                .AddTransient<IBar, Bar>()
+                .AddTransient<IBaz, Baz>()
+                .AddTransient<IGux, GuxEx>()
+                .BuildServiceProvider()
+                .GetService<IGux>();
+        }
+    }
+}
---
+++ asp.net core/ASP.NET Core 3 cnblogs/_01DependenceInjection/_DependenceInjection_07/Services.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _DependenceInjection_07
+{
+    public interface IFoo { }
+    public interface IBar { }
+    public interface IBaz { }
+    public interface IGux { }
+
+
+    public class Foo : IFoo { }
+    public class Bar : IBar { }
+    public class Baz : IBaz { }
+
+    public class Gux : IGux
+    {
+        public Gux(IFoo foo) => Console.WriteLine("Selected constructor: Gux(IFoo)");
+        public Gux(IFoo foo, IBar bar) => Console.WriteLine("Selected constructor: Gux(IFoo, IBar)");
+        public Gux(IFoo foo, IBar bar, IBaz baz) => Console.WriteLine("Selected constructor: Gux(IFoo, IBar, IBaz)");
+    }
+
+    public class GuxEx : IGux
+    {
+        public GuxEx(IFoo foo, IBar bar) => Console.WriteLine("Selected constructor: Gux(IFoo, IBar)");
+        public GuxEx(IFoo foo, IBaz baz) => Console.WriteLine("Selected constructor: Gux(IFoo,  IBaz)");
+    }
+
+}
