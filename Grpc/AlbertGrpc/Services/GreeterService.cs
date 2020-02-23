c1cbbc570b5eb275e4050447503c8b1343ffd077
Mrmantou <albert_nchu@163.com>
2020-02-23 21:21:39 +0800

    add service stream process

--- Grpc/AlbertGrpc/Services/GreeterService.cs
+++ Grpc/AlbertGrpc/Services/GreeterService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
@@ -17,10 +18,22 @@ namespace AlbertGrpc
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
+            Console.WriteLine($"request: {request.Name}");
             return Task.FromResult(new HelloReply
             {
                 Message = "Hello " + request.Name
             });
+        }
+        public override async Task SayHelloService(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            Console.WriteLine($"request: {request.Name}");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(1000);
+                HelloReply helloReply = new HelloReply { Message = $"response {i}" };
+                await responseStream.WriteAsync(helloReply);
+            }
         }
     }
 }
--- Grpc/AlbertGrpcClient/Program.cs
+++ Grpc/AlbertGrpcClient/Program.cs
@@ -1,5 +1,7 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AlbertGrpcClient
@@ -8,14 +10,28 @@ namespace AlbertGrpcClient
     {
         static async Task Main(string[] args)
         {
-
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            #region 一元调用
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 
             var client = new Greeter.GreeterClient(channel);
 
             var response = await client.SayHelloAsync(new HelloRequest { Name = "albert" });
 
             Console.WriteLine(response.Message);
+            #endregion
+
+            #region 服务器流式处理
+            var responseStream = client.SayHelloService(new HelloRequest { Name = "albert" });
+
+            var readStream = responseStream.ResponseStream;
+
+            while (await readStream.MoveNext())
+            {
+                Thread.Sleep(1000);
+                HelloReply helloReply = readStream.Current;
+                Console.WriteLine($"reply {helloReply.Message}");
+            }
+            #endregion
 
             Console.WriteLine("press any key to exit......");
             Console.ReadKey();
