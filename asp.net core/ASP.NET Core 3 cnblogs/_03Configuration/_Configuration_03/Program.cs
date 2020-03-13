80e3f8829ebcd4c317961c5e7710060bad39d6e6
Mrmantou <albert_nchu@163.com>
2020-03-13 16:50:06 +0800

    add config code

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_03Configuration/_Configuration_03/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _Configuration_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["A:B:C"] = "ABC"
+            };
+
+            var root = new ConfigurationBuilder()
+                .AddInMemoryCollection(source)
+                .Build();
+
+            var section1 = root.GetSection("A:B:C");
+            var section2 = root.GetSection("A:B").GetSection("C");
+            var section3 = root.GetSection("A").GetSection("B:C");
+
+            Debug.Assert(section1.Value == "ABC");
+            Debug.Assert(section2.Value == "ABC");
+            Debug.Assert(section3.Value == "ABC");
+
+            Debug.Assert(!ReferenceEquals(section1, section2));
+            Debug.Assert(!ReferenceEquals(section1, section3));
+            Debug.Assert(null != root.GetSection("D"));
+        }
+    }
+}
