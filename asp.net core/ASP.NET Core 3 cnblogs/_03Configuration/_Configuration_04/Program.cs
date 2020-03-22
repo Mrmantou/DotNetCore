009b340f1a7cb82171380c6ab273d2799ddffd1f
Mrmantou <albert_nchu@163.com>
2020-03-22 11:24:07 +0800

    add object bind

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_03Configuration/_Configuration_04/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _Configuration_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["foo"] = null,
+                ["bar"] = "",
+                ["baz"] = "123"
+            };
+
+            var root = new ConfigurationBuilder()
+                .AddInMemoryCollection(source)
+                .Build();
+
+            // for object
+            Debug.Assert(root.GetValue<object>("foo") == null);
+            Debug.Assert("".Equals(root.GetValue<object>("bar")));
+            Debug.Assert("123".Equals(root.GetValue<object>("baz")));
+
+            // for general type
+            Debug.Assert(root.GetValue<int>("foo") == 0);
+            Debug.Assert(root.GetValue<int>("baz") == 123);
+
+            // for Nullable<T>
+            Debug.Assert(root.GetValue<int?>("foo") == null);
+            Debug.Assert(root.GetValue<int?>("bar") == null);
+
+        }
+    }
+}
