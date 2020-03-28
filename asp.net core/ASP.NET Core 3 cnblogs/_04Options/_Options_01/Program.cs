3440f77920c26bceba9465641f32d11e2c444c4d
Mrmantou <albert_nchu@163.com>
2020-03-28 11:00:33 +0800

    add option demo

--- asp.net core/ASP.NET Core 3 cnblogs/_04Options/_Options_01/Program.cs
+++ asp.net core/ASP.NET Core 3 cnblogs/_04Options/_Options_01/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
 
 namespace _Options_01
 {
@@ -6,7 +9,58 @@ namespace _Options_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("profile.json")
+                .Build();
+            var profile = new ServiceCollection()
+                .AddOptions()
+                .Configure<Profile>(configuration)
+                .BuildServiceProvider()
+                .GetRequiredService<IOptions<Profile>>()
+                .Value;
+
+
         }
+    }
+
+    public class Profile : IEquatable<Profile>
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public ContactInfo ContactInfo { get; set; }
+
+        public Profile() { }
+        public Profile(Gender gender, int age, string emailAddress, string phoneNo)
+        {
+            Gender = gender;
+            Age = age;
+            ContactInfo = new ContactInfo
+            {
+                EmailAddress = emailAddress,
+                PhoneNo = phoneNo
+            };
+        }
+
+        public bool Equals(Profile other)
+        {
+            return other == null ? false : Gender == other.Gender && Age == other.Age && ContactInfo.Equals(other.ContactInfo);
+        }
+    }
+
+    public class ContactInfo : IEquatable<ContactInfo>
+    {
+        public string EmailAddress { get; set; }
+        public string PhoneNo { get; set; }
+
+        public bool Equals(ContactInfo other)
+        {
+            return other == null ? false : EmailAddress == other.EmailAddress && PhoneNo == other.PhoneNo;
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
     }
 }
