653fc81373245e9ea72f79f2cc178a7f31311e34
Mrmantou <albert_nchu@163.com>
2020-04-06 19:17:26 +0800

    add service hosting

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_05ServiceHosting/_ServiceHosting_01/PerformanceMetricsCollector.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _ServiceHosting_01
+{
+    public sealed class PerformanceMetricsCollector : IHostedService
+    {
+        private IDisposable scheduler;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            scheduler = new Timer(Callback, null, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+
+            static void Callback(object state)
+            {
+                Console.WriteLine($"[{DateTimeOffset.Now}] {PerformanceMetrics.Create()}");
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            scheduler.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ asp.net core/ASP.NET Core 3 cnblogs/_05ServiceHosting/_ServiceHosting_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace _ServiceHosting_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HostBuilder()
+                .ConfigureServices(services =>
+                    //services.AddSingleton<IHostedService, PerformanceMetricsCollector>())
+                    services.AddHostedService<PerformanceMetricsCollector>())
+                .Build()
+                .Run();
+        }
+    }
+}
