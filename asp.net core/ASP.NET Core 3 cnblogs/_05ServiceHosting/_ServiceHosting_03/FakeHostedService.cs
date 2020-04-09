0dcfc20baff25bac612829974b59ffa7b687d98f
Mrmantou <albert_nchu@163.com>
2020-04-09 22:00:38 +0800

    add application life time demo

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_05ServiceHosting/_ServiceHosting_03/FakeHostedService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _ServiceHosting_03
+{
+    public class FakeHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime lifetime;
+        private IDisposable tokenSource;
+
+        public FakeHostedService(IHostApplicationLifetime lifetime)
+        {
+            this.lifetime = lifetime;
+            this.lifetime.ApplicationStarted.Register(() => Console.WriteLine($"[{DateTimeOffset.Now}] Application started"));
+            this.lifetime.ApplicationStopping.Register(() => Console.WriteLine($"[{DateTimeOffset.Now}] Application is stopping"));
+            this.lifetime.ApplicationStopped.Register(() => Console.WriteLine($"[{DateTimeOffset.Now}] Application stoped"));
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            tokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5)).Token.Register(lifetime.StopApplication);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            tokenSource.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ asp.net core/ASP.NET Core 3 cnblogs/_05ServiceHosting/_ServiceHosting_03/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _ServiceHosting_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HostBuilder()
+                .ConfigureServices(services => services.AddHostedService<FakeHostedService>())
+                .Build()
+                .Run();
+        }
+    }
+}
