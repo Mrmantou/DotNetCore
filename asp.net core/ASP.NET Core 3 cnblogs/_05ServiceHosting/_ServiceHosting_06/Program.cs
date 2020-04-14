a9a115ddc06707cc4201e4cdd7cbc01f5a2273f8
Mrmantou <albert_nchu@163.com>
2020-04-14 21:36:32 +0800

    add host environment config

---
+++ asp.net core/ASP.NET Core 3 cnblogs/_05ServiceHosting/_ServiceHosting_06/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _ServiceHosting_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HostBuilder()
+                .ConfigureHostConfiguration(builder => builder.AddCommandLine(args))
+                .ConfigureServices(services => services.AddHostedService<FakeHostedService>())
+                .Build()
+                .Run();
+
+            /*
+             dotnet run /environment=develop /contentroot=d:/temp /applicationname=App
+             EnvironmentName:develop
+             ApplicationName:App
+             ContentRootPath:d:/temp
+             */
+        }
+    }
+}
