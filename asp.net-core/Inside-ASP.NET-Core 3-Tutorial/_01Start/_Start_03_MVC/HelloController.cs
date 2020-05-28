9d3467520380c47c371c59881183b818a7920f9a
Mrmantou <albert_nchu@163.com>
2020-05-28 11:20:45 +0800

    add view

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_01Start/_Start_03_MVC/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _Start_03_MVC
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("/hello/{name}")]
+        public IActionResult SayHello(string name)
+        {
+            ViewBag.Name = name;
+            return View();
+        }
+    }
+}
