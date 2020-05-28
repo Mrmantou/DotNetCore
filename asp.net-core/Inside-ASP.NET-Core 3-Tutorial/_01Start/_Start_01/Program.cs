9f69063b66287079990ac11f22e91da336c05430
Mrmantou <albert_nchu@163.com>
2020-05-28 11:06:51 +0800

    configure web host

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_01Start/_Start_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _Start_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHost(builder => builder
+                    .UseKestrel()
+                    .Configure(app => app
+                        .Run(context => context.Response.WriteAsync("Hello World"))))
+                .Build()
+                .Run();
+        }
+    }
+}
