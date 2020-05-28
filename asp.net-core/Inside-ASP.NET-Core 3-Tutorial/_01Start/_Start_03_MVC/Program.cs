025629cfed64ef0259f764df57ada5956916e4fb
Mrmantou <albert_nchu@163.com>
2020-05-28 11:08:14 +0800

    add and register mvc middle

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_01Start/_Start_03_MVC/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _Start_03_MVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder => builder
+                    .ConfigureServices(services => services
+                        .AddRouting()
+                        .AddControllersWithViews())
+                    .Configure(app => app
+                        .UseRouting() //Adds a Microsoft.AspNetCore.Routing.EndpointRoutingMiddleware middleware
+                        .UseEndpoints(endpoints => endpoints.MapControllers()))) //Adds a Microsoft.AspNetCore.Routing.EndpointMiddleware middleware 
+                .Build()
+                .Run();
+        }
+    }
+}
