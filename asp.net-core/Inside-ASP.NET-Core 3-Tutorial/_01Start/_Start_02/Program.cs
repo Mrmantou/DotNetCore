45cf48f5cd6aaa075de72b2b325818207c184e8a
Mrmantou <albert_nchu@163.com>
2020-05-28 11:07:39 +0800

    configure web host default

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_01Start/_Start_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _Start_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Host.CreateDefaultBuilder()
+            //    .ConfigureWebHost(builder => builder
+            //        .UseKestrel()
+            //        .UseUrls("http://0.0.0.0:6100;https://0.0.0.0:6101")
+            //        .Configure(app => app
+            //            .Run(context => context.Response.WriteAsync("Hello World"))))
+            //    .Build()
+            //    .Run();
+
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder => builder
+                    .Configure(app => app
+                        .Run(context => context.Response.WriteAsync("Hello World"))))
+                .Build()
+                .Run();
+        }
+    }
+}
