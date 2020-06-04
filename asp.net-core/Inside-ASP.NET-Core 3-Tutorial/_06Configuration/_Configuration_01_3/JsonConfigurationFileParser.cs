f3a5cd5f7b6f2cd4e0c700acbc1db64eb69ed4c3
Mrmantou <albert_nchu@163.com>
2020-06-04 10:08:47 +0800

    add json configuration file parser

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_06Configuration/_Configuration_01_3/JsonConfigurationFileParser.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace _Configuration_01_3
+{
+    class JsonConfigurationFileParser
+    {
+        private JsonConfigurationFileParser() { }
+
+        private readonly IDictionary<string, string> _data = new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private readonly Stack<string> _context = new Stack<string>();
+        private string _currentPath;
+
+        public static IDictionary<string, string> Parse(Stream input)
+            => new JsonConfigurationFileParser().ParseStream(input);
+
+        private IDictionary<string, string> ParseStream(Stream input)
+        {
+            _data.Clear();
+
+            var jsonDocumentOptions = new JsonDocumentOptions
+            {
+                CommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+            };
+
+            using (var reader = new StreamReader(input))
+            using (JsonDocument doc = JsonDocument.Parse(reader.ReadToEnd(), jsonDocumentOptions))
+            {
+                if (doc.RootElement.ValueKind != JsonValueKind.Object)
+                {
+                    throw new FormatException(Resources.FormatError_UnsupportedJSONToken(doc.RootElement.ValueKind));
+                }
+                VisitElement(doc.RootElement);
+            }
+
+            return _data;
+        }
+
+        private void VisitElement(JsonElement element)
+        {
+            var properties = element.EnumerateObject();
+            foreach (var property in properties)
+            {
+                EnterContext(property.Name);
+                VisitValue(property.Value);
+                ExitContext();
+            }
+        }
+
+        private void VisitValue(JsonElement value)
+        {
+            switch (value.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    VisitElement(value);
+                    break;
+
+                case JsonValueKind.Array:
+                    var index = 0;
+                    foreach (var arrayElement in value.EnumerateArray())
+                    {
+                        EnterContext(index.ToString());
+                        VisitValue(arrayElement);
+                        ExitContext();
+                        index++;
+                    }
+                    break;
+
+                case JsonValueKind.Number:
+                case JsonValueKind.String:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Null:
+                    var key = _currentPath;
+                    if (_data.ContainsKey(key))
+                    {
+                        throw new FormatException(Resources.FormatError_KeyIsDuplicated(key));
+                    }
+                    _data[key] = value.ToString();
+                    break;
+
+                default:
+                    throw new FormatException(Resources.FormatError_UnsupportedJSONToken(value.ValueKind));
+            }
+        }
+
+        private void EnterContext(string context)
+        {
+            _context.Push(context);
+            _currentPath = ConfigurationPath.Combine(_context.Reverse());
+        }
+
+        private void ExitContext()
+        {
+            _context.Pop();
+            _currentPath = ConfigurationPath.Combine(_context.Reverse());
+        }
+    }
+}
---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_06Configuration/_Configuration_01_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace _Configuration_01_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new FileStream("appsettings.json", FileMode.Open);
+            var config = JsonConfigurationFileParser.Parse(stream);
+        }
+    }
+}
