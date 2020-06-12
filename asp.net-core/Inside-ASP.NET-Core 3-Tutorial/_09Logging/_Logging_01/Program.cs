84cf4d28dfa3dfd9675da5b4ad8db2361c38aa15
Mrmantou <albert_nchu@163.com>
2020-06-12 19:23:54 +0800

    add logging demo

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_09Logging/_Logging_01/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _Logging_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var logger = new ServiceCollection()
+            //    .AddLogging(builder => builder
+            //        .AddConsole()
+            //        .AddDebug())
+            //    .BuildServiceProvider()
+            //    .GetRequiredService<ILoggerFactory>()
+            //    .CreateLogger("_Logging_01.Program");
+
+            //var logger = new ServiceCollection()
+            //    .AddLogging(builder => builder
+            //        .AddConsole()
+            //        .AddDebug())
+            //    .BuildServiceProvider()
+            //    .GetRequiredService<ILogger<Program>>();
+
+            var listener = new FoobarEventListener();
+
+            listener.EventSourceCreated += (sender, args) =>
+            {
+                if (args.EventSource.Name == "Microsoft-Extension-Logging")
+                {
+                    listener.EnableEvents(args.EventSource, EventLevel.LogAlways);
+                }
+            };
+
+            listener.EventWritten += (sender, args) =>
+            {
+                if (args.EventName == "FormattedMessage")
+                {
+                    var payload = args.Payload;
+                    var payloadNames = args.PayloadNames;
+                    var indexOfLevel = payloadNames.IndexOf("Level");
+                    var indexOfCategory = args.PayloadNames.IndexOf("LoggerName");
+                    var indexOfEventId = args.PayloadNames.IndexOf("EventId");
+                    var indexOfMessage = args.PayloadNames.IndexOf("FormattedMessage");
+
+                    Console.WriteLine($"{(LogLevel)payload[indexOfLevel],-11}: {payload[indexOfCategory]}[{payload[indexOfEventId]}]");
+                    Console.WriteLine($"{"",-13}{payload[indexOfMessage]}");
+                }
+            };
+
+            var logger = new ServiceCollection()
+                .AddLogging(builder => builder
+                    .AddTraceSource(
+                        new SourceSwitch("default", "All"),
+                        new DefaultTraceListener { LogFileName = "trace.log" })
+                    .AddEventSourceLogger())
+                .BuildServiceProvider()
+                .GetRequiredService<ILogger<Program>>();
+
+
+            var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
+            levels = levels.Where(l => l != LogLevel.None).ToArray();
+            var eventId = 1;
+            Array.ForEach(levels, level => logger.Log(level, eventId++, $"This is a/an {level} log message"));
+
+            Console.WriteLine("press any to exit......");
+            Console.ReadKey();
+        }
+
+        private class FoobarEventListener : EventListener { }
+    }
+}
---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_09Logging/_Logging_02/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _Logging_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var logger = new ServiceCollection()
+            //    .AddLogging(builder => builder
+            //        .SetMinimumLevel(LogLevel.Trace)
+            //        .AddConsole())
+            //    .BuildServiceProvider()
+            //    .GetRequiredService<ILogger<Program>>();
+
+            var logger = new ServiceCollection()
+                .AddLogging(builder => builder
+                    .AddFilter(Filter)
+                    .AddConsole())
+                .BuildServiceProvider()
+                .GetRequiredService<ILogger<Program>>();
+
+            var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
+            levels = levels.Where(l => l != LogLevel.None).ToArray();
+            var eventId = 1;
+            Array.ForEach(levels, level => logger.Log(level, eventId++, $"This is a/an {level} log message"));
+
+            Console.WriteLine("press any key to exit......");
+            Console.ReadKey();
+
+            static bool Filter(string category, LogLevel level)
+            {
+                return category switch
+                {
+                    "Foo" => level >= LogLevel.Debug,
+                    "Bar" => level >= LogLevel.Warning,
+                    "Baz" => level >= LogLevel.None,
+                    _ => level >= LogLevel.Information
+                };
+            }
+        }
+
+        static void SetLevel()
+        {
+            var logger = new ServiceCollection()
+                .AddLogging(builder => builder
+                    .SetMinimumLevel(LogLevel.Trace)
+                    .AddConsole())
+                .BuildServiceProvider()
+                .GetRequiredService<ILogger<Program>>();
+
+            var levels = (LogLevel[])Enum.GetValues(typeof(LogLevel));
+            levels = levels.Where(l => l != LogLevel.None).ToArray();
+            var eventId = 1;
+            Array.ForEach(levels, level => logger.Log(level, eventId++, $"This is a/an {level} log message"));
+        }
+    }
+}
