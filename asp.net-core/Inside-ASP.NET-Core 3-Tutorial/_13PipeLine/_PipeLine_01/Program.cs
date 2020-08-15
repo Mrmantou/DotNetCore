9777f6e395b28af6feed3db23542cc2568f7af21
Mrmantou <albert_nchu@163.com>
2020-08-15 19:16:59 +0800

    add pipeline demo

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_13PipeLine/_PipeLine_01/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace _PipeLine_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureLogging(builder => builder
+                    .AddConsole(options => options.IncludeScopes = true))
+                .ConfigureWebHostDefaults(builder => builder
+                    .Configure(app => app.Run(context =>
+                    {
+                        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+                        logger.LogInformation("Log for event Foobar");
+
+                        if (context.Request.Path == new PathString("/error"))
+                        {
+                            throw new InvalidOperationException("Manually, throw exception.");
+                        }
+
+                        return Task.CompletedTask;
+                    })))
+                .Build()
+                .Run();
+        }
+    }
+}
