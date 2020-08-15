6d0605cfa067b332893938fb094153810b623917
Mrmantou <albert_nchu@163.com>
2020-08-15 19:17:15 +0800

    add static file demo

---
+++ asp.net-core/Inside-ASP.NET-Core 3-Tutorial/_14StaticFile/_StaticFile_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace _StaticFile_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(builder => builder
+                    .Configure(app => app
+                        .UseStaticFiles()))
+                .Build()
+                .Run();
+        }
+    }
+}
