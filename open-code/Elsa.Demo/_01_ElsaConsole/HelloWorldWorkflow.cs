c7881117cb9342ea36598b17ead3bd5e7c04d6f6
Mrmantou <albert_nchu@163.com>
2021-07-01 13:07:46 +0800

    add a elsa demo

---
+++ open-code/Elsa.Demo/_01_ElsaConsole/HelloWorldWorkflow.cs
@@ -0,0 +1,10 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Builders;
+
+namespace _01_ElsaConsole
+{
+    public class HelloWorld : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)=>builder.WriteLine("Hello World!");
+    }
+}
---
+++ open-code/Elsa.Demo/_01_ElsaConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using Elsa.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace _01_ElsaConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var services = new ServiceCollection()
+                .AddElsa(options => options
+                    .AddConsoleActivities()
+                    .AddWorkflow<HelloWorld>())
+                .BuildServiceProvider();
+
+            var workflowRunner = services.GetRequiredService<IBuildsAndStartsWorkflow>();
+
+            await workflowRunner.BuildAndStartWorkflowAsync<HelloWorld>();
+
+            Console.WriteLine("press any key to exit......");
+        }
+    }
+}
